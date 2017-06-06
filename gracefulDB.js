9fab81916f7d3b06e619e685a55b1ed4db7a510a
Benjirez <benjirez@gmail.com>
2017-06-06 12:44:24 -0700

    Create gracefulDB.js

---
+++ gracefulDB.js
@@ -0,0 +1,45 @@
+// require this in your app.js (see getting MEAN p.129)
+
+var mongoose = require( 'mongoose' );
+
+//var dbURI = 'mongodb://localhost/Loc8r';
+//mongoose.connect(dbURI);
+//mongoose.connection.on('connected', function () {
+// console.log('Mongoose connected to ' + dbURI);
+//});
+
+var dbLoc = 'mongodb://' + process.env.IP + '/ben1';
+var db = mongoose.connect(dbLoc);
+
+
+
+mongoose.connection.on('error',function (err) {
+ console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+ console.log('Mongoose disconnected');
+});
+gracefulShutdown = function (msg, callback) {
+ mongoose.connection.close(function () {
+ console.log('Mongoose disconnected through ' + msg);
+ callback();
+ });
+};
+// For nodemon restarts
+process.once('SIGUSR2', function () {
+ gracefulShutdown('nodemon restart', function () {
+ process.kill(process.pid, 'SIGUSR2');
+ });
+});
+// For app termination
+process.on('SIGINT', function() {
+ gracefulShutdown('app termination', function () {
+ process.exit(0);
+ });
+});
+// For Heroku app termination
+process.on('SIGTERM', function() {
+ gracefulShutdown('Heroku app shutdown', function () {
+ process.exit(0);
+ });
+}); 
